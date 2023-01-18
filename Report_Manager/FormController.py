ff849d2ac51f938775699a75474a58c07a1ed2bd
suli98suliman998 <suli.braim998@gmail.com>
2023-01-18 21:36:31 +0300

    pre data

--- Report_Manager/FormController.py
+++ Report_Manager/FormController.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 
 from Report_Manager.FormModel import get_form_columns_by_form_id
 from Report_Manager.TemplateModel import get_row_titles_by_template_id
@@ -11,11 +11,8 @@ def controller_daily_barn_pre_form():
         cycle_number = request.form.get("cycle_number")
         shift = request.form.get("shift")
         date = datetime.date
-        print(date)
-        print(cycle_number)
-        print(shift)
-        column_names = get_form_columns_by_form_id(1)
-        row_names = get_row_titles_by_template_id(1)
-        return render_template('form_builder.html', column_names=column_names, row_names=row_names)
+
+        rows = get_row_titles_by_template_id(1)
+        return redirect(url_for('view_labor_form', columns=shift, rows=rows))
     shifts = ["6:00 -> 9:00", "9:00 -> 12:00", "12:00 -> 15:00", "15:00 -> 18:00"]
     return render_template('daily_barn_pre_data.html', shifts=shifts)
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import jsonify, render_template
-
+from flask import jsonify, render_template, request
 
 from Report_Manager.TemplateModel import get_row_titles_by_template_id
 
@@ -13,13 +12,15 @@ def get_template_rows(template_id):
     return jsonify(row_titles)
 
 
-# @app.route('/labor_form')
-# def view_labor_form():
-#     return build_form(1)
+@app.route('/labor_form')
+def view_labor_form():
+    columns = request.args.get("columns")
+    rows = request.args.get("rows")
+    return render_template('form_builder.html', column_names=columns, row_names=rows)
 
 
 @app.route('/labor_pre_form', methods=['GET', 'POST'])
-def view_labor_form():
+def view_labor_pre_form():
     from Report_Manager.FormController import controller_daily_barn_pre_form
     return controller_daily_barn_pre_form()
 
